03d67735f2b317e17e7bdc34e203773da4ca9dd3
Miguel Rodo <rdxmig002@myuct.ac.za>
2024-02-20 14:37:36 +0200

    Fix bug where env var was assumed lower case

--- src/repos/install.sh
+++ src/repos/install.sh
@@ -214,7 +214,7 @@ clone_repos() {
     }
 
     # If running in a Codespace, set up Git credentials
-    if [ "${override_credential_helper}" ]; then
+    if [ "${OVERRIDE_CREDENTIAL_HELPER}" ]; then
         # Remove the default credential helper
         sudo sed -i -E 's/helper =.*//' /etc/gitconfig
 
