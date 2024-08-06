f8ad2a1ace8e6818949bf0ff2866528c2c78a084
Miguel Rodo <rdxmig002@myuct.ac.za>
2024-08-06 13:49:30 +0200

    Add FIt-SNE feature

---
+++ src/fftRtsne/README.md
@@ -0,0 +1,5 @@
+
+# Install FIt-SNE
+
+As per here: https://github.com/KlugerLab/FIt-SNE.
+
---
+++ src/fftRtsne/install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Source: https://xethub.com/assets/docs/getting-started/install
+
+set -e
+
+# Save current directory
+ORIGINAL_DIR=$(pwd)
+
+# Switch to /tmp directory
+pushd /tmp
+
+# Install FFTW
+wget http://www.fftw.org/fftw-3.3.10.tar.gz
+tar -xzf fftw-3.3.10.tar.gz
+cd fftw-3.3.10
+./configure --prefix=$HOME/fftw
+make
+make install
+
+# Clean up FFTW files
+cd /tmp
+rm -rf fftw-3.3.10 fftw-3.3.10.tar.gz
+
+# Install FIt-SNE
+git clone https://github.com/KlugerLab/FIt-SNE
+cd FIt-SNE
+g++ -std=c++11 -O3 src/sptree.cpp src/tsne.cpp src/nbodyfft.cpp -o fast_tsne -pthread -I$HOME/fftw/include -L$HOME/fftw/lib -lfftw3 -lm -Wno-address-of-packed-member
+
+# Move the fast_tsne executable to /usr/local/bin
+sudo mv fast_tsne /usr/local/bin
+
+# Clean up FIt-SNE files
+cd /tmp
+rm -rf FIt-SNE
+
+# Return to the original directory
+popd
+
+# Verify the return to the original directory
+echo "Back to directory: $(pwd)"
+
+# Verify installation
+if command -v fast_tsne &> /dev/null; then
+    echo "fast_tsne installed successfully"
+else
+    echo "fast_tsne installation failed"
+fi
