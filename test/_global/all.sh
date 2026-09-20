31dfad510e0b48897880d1388241c9af6532da46
Miguel Rodo <miguel.rodo@outlook.com>
2026-09-20 10:49:17 +0200

    Merge current main test coverage into utils checks

--- test/_global/all.sh
+++ test/_global/all.sh
@@ -21,6 +21,18 @@ check "setupmjr help command works" setupmjr --help
 check "fast_tsne binary is installed" bash -c "command -v fast_tsne"
 check "fast_tsne is executable" bash -c "test -x /usr/local/bin/fast_tsne"
 check "FFTW library is installed" bash -c "ldconfig -p | grep fftw3"
+check "fast_tsne processes a tiny dataset" bash -c '
+set -e
+tmp=$(mktemp -d)
+# FIt-SNE 1.2.1 fixture: 12 deterministic 2D points, perplexity 2, two iterations.
+printf "%s" "DAAAAAIAAAAAAAAAAADgPwAAAAAAAABAAgAAAAIAAAABAAAAAQAAAAAAAAAAAOA/mpmZmZmZ6T8AAAAAAABpQAAAAAAAABRA/////wAAAAAAAD7AAgAAAAIAAAAAAAAAAAAoQAAAAAABAAAABgAAAP////8AAAAAAADwvwMAAAAAAAAAAADwPwoAAAAAAAAAAAAAwAAAAAAAAADAAAAAAAAAAMAAAAAAAADwvwAAAAAAAPC/AAAAAAAAAMAAAAAAAADwvwAAAAAAAPC/AAAAAAAA8D8AAAAAAADwPwAAAAAAAPA/AAAAAAAAAEAAAAAAAAAAQAAAAAAAAPA/AAAAAAAAAEAAAAAAAAAAQAAAAAAAAADAAAAAAAAAAEAAAAAAAADwvwAAAAAAAPA/AAAAAAAA8D8AAAAAAADwvwAAAAAAAABAAAAAAAAAAMAqAAAAAAAAAAAA8D8AAAAA" | base64 -d > "$tmp/data.dat"
+fast_tsne 1.2.1 "$tmp/data.dat" "$tmp/result.dat" 1
+test "$(wc -c < "$tmp/result.dat")" -eq 220
+read -r n d < <(od -An -N8 -t u4 "$tmp/result.dat")
+test "$n" -eq 12
+test "$d" -eq 2
+rm -rf "$tmp"
+'
 # Apptainer tests
 check "apptainer binary is installed" bash -c "command -v apptainer"
 check "apptainer version command works" bash -c "apptainer --version"
