343b28de9f9bc3dcd8e110f46f3d4760a449f66c
Miguel Rodo <miguel.rodo@outlook.com>
2026-09-20 11:44:06 +0200

    test: cover nested feature documentation exemption

--- .github/scripts/check-feature-version-bumps.test.sh
+++ .github/scripts/check-feature-version-bumps.test.sh
@@ -10,7 +10,7 @@ cd "${tmp}"
 git init -q
 git config user.email test@example.com
 git config user.name test
-mkdir -p src/a src/b
+mkdir -p src/a/docs src/b
 printf '%s\n' '{"version":"1.0.0"}' > src/a/devcontainer-feature.json
 printf '%s\n' '{"version":"1.0.0"}' > src/b/devcontainer-feature.json
 printf '%s\n' 'echo a' > src/a/install.sh
@@ -18,7 +18,7 @@ printf '%s\n' 'echo b' > src/b/install.sh
 git add . && git commit -qm initial
 initial=$(git rev-parse HEAD)
 
-printf '%s\n' '# docs' > src/a/README.md
+printf '%s\n' '# docs' > src/a/docs/README.md
 git add . && git commit -qm docs
 docs=$(git rev-parse HEAD)
 bash "${guard}" "${initial}" "${docs}"
